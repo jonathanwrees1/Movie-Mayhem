46f64a7aa36ef1ad435956f573477ea08988eadc
jonathanwrees1 <97190782+jonathanwrees1@users.noreply.github.com>
2022-02-16 15:00:01 -0800

    Added detail to index.js and adjusted documentation.html accordingly

--- index.js
+++ index.js
@@ -11,118 +11,226 @@ let users = [
     id: 1,
     name: 'Kim',
     favoriteMovies: [],
+    dateOfBirth: 'January 15, 2001',
+    email: 'kimmyjones@yahoo.com',
   },
   {
     id: 2,
     name: 'Joe',
     favoriteMovies: ['Rogue One: A Star Wars Story'],
+    dateOfBirth: 'October 11, 1992',
+    email: 'joeyouknow23@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Mary',
+    favoriteMovies: ['Episode 1: The Phantom Menace'],
+    dateOfBirth: 'November 5, 1988',
+    email: 'quitecontrary33@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Jackson',
+    favoriteMovies: [],
+    dateOfBirth: 'March 3, 1995',
+    email: 'notmichael99@yahoo.com',
   },
 ];
 
 let starWars = [
   {
     title: 'Episode 1: The Phantom Menace',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/6wkfovpn7Eq8dYNKaG5PY3q2oq6.jpg',
+    overview:
+      'Anakin Skywalker, a young slave strong with the Force, is discovered on Tatooine by Jedis Qui-Gon Jinn and Obi-Wan Kenobi. Meanwhile, the evil Sith have returned, enacting their plot for revenge against the Jedi.',
     director: {
       name: 'George Lucas',
+      bio: "George Walton Lucas Jr.[1] (born May 14, 1944) is an American film director, producer, screenwriter, and entrepreneur. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, Lucasfilm Games, and Industrial Light & Magic. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012.[2] Lucas is one of history's most financially successful filmmakers and has been nominated for four Academy Awards. His films are among the 100 highest-grossing movies at the North American box office, adjusted for ticket-price inflation.[3] Lucas is considered one of the most significant figures of the 20th-century New Hollywood movement, and a pioneer of the modern blockbuster.",
+      birthYear: '1944',
+      deathYear: '',
     },
-    released: '1999',
+    released: '5/19/1999',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 2: Attack Of The Clones',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/6wkfovpn7Eq8dYNKaG5PY3q2oq6.jpg',
+    overview:
+      'Following an assassination attempt on Senator Padmé Amidala, Jedi Knights Anakin Skywalker and Obi-Wan Kenobi further investigate the true identity of the Sith, and discover a mysterious plan that was set in motion many years earlier. Anakin and Padme grow closer in their affection for eachother, even though attachment is forbidden for a Jedi',
     director: {
       name: 'George Lucas',
+      bio: "George Walton Lucas Jr.[1] (born May 14, 1944) is an American film director, producer, screenwriter, and entrepreneur. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, Lucasfilm Games, and Industrial Light & Magic. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012.[2] Lucas is one of history's most financially successful filmmakers and has been nominated for four Academy Awards. His films are among the 100 highest-grossing movies at the North American box office, adjusted for ticket-price inflation.[3] Lucas is considered one of the most significant figures of the 20th-century New Hollywood movement, and a pioneer of the modern blockbuster.",
+      birthYear: '1944',
+      deathYear: '',
     },
-    released: '2002',
+    released: '05/16/2002 ',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 3: Revenge Of the Sith',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/xfSAoBEm9MNBjmlNcDYLvLSMlnq.jpg',
+    overview:
+      "The evil Darth Sidious enacts his final plan for unlimited power. Once the Sith Lord's true identity is discovered, Anakin Skywalker is forced to choose a side.",
     director: {
       name: 'George Lucas',
+      bio: "George Walton Lucas Jr.[1] (born May 14, 1944) is an American film director, producer, screenwriter, and entrepreneur. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, Lucasfilm Games, and Industrial Light & Magic. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012.[2] Lucas is one of history's most financially successful filmmakers and has been nominated for four Academy Awards. His films are among the 100 highest-grossing movies at the North American box office, adjusted for ticket-price inflation.[3] Lucas is considered one of the most significant figures of the 20th-century New Hollywood movement, and a pioneer of the modern blockbuster.",
+      birthYear: '1944',
+      deathYear: '',
     },
-    released: '2005',
+    released: '5/19/2005',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Rogue One: A Star Wars Story',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/i0yw1mFbB7sNGHCs7EXZPzFkdA1.jpg',
+    overview:
+      'Jyn Erso and a rogue band of resistance fighters unite for a mission against The Empire to steal the Death Star plans and bring hope to the galaxy.',
     director: {
       name: 'Gareth Edwards',
+      bio: "Gareth James Edwards (born 13 July 1975) is a British visual effects artist, screenwriter, film director, cinematographer, production designer, and film producer. He first gained widespread recognition for Monsters (2010), an independent film in which he served as writer, director, cinematographer, and visual effects artist.[1][2] He subsequently directed Godzilla (2014), a reboot[3] of Toho's Godzilla franchise and the first film in Legendary's MonsterVerse, and Rogue One: A Star Wars Story (2016), the first installment of the Star Wars anthology series and an immediate prequel to Star Wars: Episode IV – A New Hope (1977).",
+      birthYear: '1975',
+      deathYear: '',
     },
-    released: '2016',
+    released: '12/16/2016',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 4: A New Hope',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/eFPSzaRX9K1tfUpmBxBuDGFjULs.jpg',
+    overview:
+      'Princess Leia Organa is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Luke Skywalker and captain Han Solo team together with the robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.',
     director: {
       name: 'George Lucas',
+      bio: "George Walton Lucas Jr.[1] (born May 14, 1944) is an American film director, producer, screenwriter, and entrepreneur. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, Lucasfilm Games, and Industrial Light & Magic. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012.[2] Lucas is one of history's most financially successful filmmakers and has been nominated for four Academy Awards. His films are among the 100 highest-grossing movies at the North American box office, adjusted for ticket-price inflation.[3] Lucas is considered one of the most significant figures of the 20th-century New Hollywood movement, and a pioneer of the modern blockbuster.",
+      birthYear: '1944',
+      deathYear: '',
     },
-    released: '1977',
+    released: '05/25/1977',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 5: The Empire Strikes Back',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/7BuH8itoSrLExs2YZSsM01Qk2no.jpg',
+    overview:
+      'The saga continues as Luke Skywalker, learns the ways of the Jedi from aging master Yoda. But Darth Vader is more determined than ever to capture Luke. Meanwhile, rebel leader Princess Leia, Han Solo, Chewbacca, and droids C-3PO and R2-D2 are thrown into various stages of capture, betrayal and despair.',
     director: {
-      name: 'Irvine Kershner',
+      name: 'Irvin Kershner',
+      bio: "Irvin Kershner (born Isadore Kershner; April 29, 1923 – November 27, 2010) was an American director, actor, and producer of film and television. He gained notice early in his career as a filmmaker for directing quirky, independent drama films, while working as an influential lecturer at the University of Southern California. Later in his career, he transitioned to high-budget blockbusters such as The Empire Strikes Back, the James Bond adaptation Never Say Never Again, and RoboCop 2. Through the course of his career, he received numerous accolades, and was nominated for both a Primetime Emmy Award and a Palme d'Or.",
+      birthYear: '1923',
+      deathYear: '2010',
     },
-    released: '1980',
+    released: '6/20/1980',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 6: Return Of The Jedi',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/ydjp1K13GrnbiX0yjd398BI9xaC.jpg',
+    overview:
+      'Luke Skywalker leads a mission to rescue his friend Han Solo from the clutches of Jabba the Hutt, while the Emperor (aka: Darth Sidious) seeks to destroy the Rebellion once and for all with a second Death Star.',
     director: {
       name: 'George Lucas',
+      bio: "George Walton Lucas Jr.[1] (born May 14, 1944) is an American film director, producer, screenwriter, and entrepreneur. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, Lucasfilm Games, and Industrial Light & Magic. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012.[2] Lucas is one of history's most financially successful filmmakers and has been nominated for four Academy Awards. His films are among the 100 highest-grossing movies at the North American box office, adjusted for ticket-price inflation.[3] Lucas is considered one of the most significant figures of the 20th-century New Hollywood movement, and a pioneer of the modern blockbuster.",
+      birthYear: '1944',
+      deathYear: '',
     },
-    released: '1983',
+    released: '5/25/1983',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 7: The Force Awakens',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/wqnLdwVXoBjKibFRR5U3y0aDUhs.jpg',
+    overview:
+      'Thirty years after defeating the Galactic Empire, Han Solo and his allies face a new threat from the evil Kylo Ren and his army of First Order Stormtroopers. This installment introduces us to a brand new, force-sensitive heroine, Rey.',
     director: {
       name: 'J.J Abrams',
+      bio: ' Jeffrey Jacob Abrams (born June 27, 1966)[1] is an American filmmaker and composer. He is best known for his works in the genres of action, drama, and science fiction. Abrams wrote and produced such films as Regarding Henry (1991), Forever Young (1992), Armageddon (1998), Cloverfield (2008), Star Trek (2009), Star Wars: The Force Awakens (2015), and Star Wars: The Rise of Skywalker (2019). Abrams has created numerous television series, including Felicity (co-creator, 1998–2002), Alias (creator, 2001–2006), Lost (co-creator, 2004–2010), and Fringe (co-creator, 2008–2013). He won two Emmy Awards for Lost – Outstanding Directing for a Drama Series and Outstanding Drama Series. His directorial film work includes Mission: Impossible III (2006), Star Trek (2009), Super 8 (2011), and Star Trek Into Darkness (2013). He also directed, produced and co-wrote The Force Awakens, the seventh episode of the Star Wars saga and the first film of the sequel trilogy. The film is his highest-grossing, as well as the fourth-highest-grossing film of all time not adjusted for inflation. He returned to Star Wars by executive producing The Last Jedi (2017), and directing and co-writing The Rise of Skywalker (2019).[2]',
+      birthYear: '1966',
+      deathYear: '',
     },
-    released: '2015',
+    released: '12/18/2015',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 8: The Last Jedi',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg',
+    overview:
+      'Rey strengthens her newly discovered abilities under the guidance of Luke Skywalker, who is very concerned about the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
     director: {
       name: 'Rian Johnson',
+      bio: 'Rian Craig Johnson (born December 17, 1973) is an American filmmaker. He made his directorial debut with the neo-noir mystery film Brick (2005), which received positive reviews and grossed nearly $4 million on a $450,000 budget. Transitioning to higher-profile films, Johnson achieved mainstream recognition for writing and directing the science-fiction thriller Looper (2012) to critical and commercial success. Johnson landed his largest project when he wrote and directed the space opera Star Wars: The Last Jedi (2017), which grossed over $1 billion. He returned to the mystery genre with Knives Out (2019), earning him an Academy Award nomination for Best Original Screenplay.',
+      birthYear: '1973',
+      deathYear: '',
     },
-    released: '2017',
+    released: '12/15/2017',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
   {
     title: 'Episode 9: The Rise Of Skywalker',
+    imageUrl:
+      'https://www.themoviedb.org/t/p/original/db32LaOibwEliAmSL2jjDF6oDdj.jpg',
+    overview:
+      'The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. Jedi Rey learns the truth about her forgotten past, and must come to terms with who she wants to be.',
     director: {
       name: 'J.J. Abrams',
+      bio: 'Jeffrey Jacob Abrams (born June 27, 1966)[1] is an American filmmaker and composer. He is best known for his works in the genres of action, drama, and science fiction. Abrams wrote and produced such films as Regarding Henry (1991), Forever Young (1992), Armageddon (1998), Cloverfield (2008), Star Trek (2009), Star Wars: The Force Awakens (2015), and Star Wars: The Rise of Skywalker (2019). Abrams has created numerous television series, including Felicity (co-creator, 1998–2002), Alias (creator, 2001–2006), Lost (co-creator, 2004–2010), and Fringe (co-creator, 2008–2013). He won two Emmy Awards for Lost – Outstanding Directing for a Drama Series and Outstanding Drama Series. His directorial film work includes Mission: Impossible III (2006), Star Trek (2009), Super 8 (2011), and Star Trek Into Darkness (2013). He also directed, produced and co-wrote The Force Awakens, the seventh episode of the Star Wars saga and the first film of the sequel trilogy. The film is his highest-grossing, as well as the fourth-highest-grossing film of all time not adjusted for inflation. He returned to Star Wars by executive producing The Last Jedi (2017), and directing and co-writing The Rise of Skywalker (2019).[2]',
+      birthYear: '1966',
+      deathYear: '',
     },
-    released: '2019',
+    released: '12/20/2019',
     genre: {
       name: 'Sci-Fi',
+      description:
+        'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations.',
     },
   },
 ];
 
-//Middleware:Morga Logging Package
+//Middleware:Morgan Logging Package
 app.use(morgan('common'));
 
 //CREATE new user
